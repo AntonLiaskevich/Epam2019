b033a95c3082e0e1838d84e61c5225649a89d768
AntonLiaskevich <antlon.lesk@gmail.com>
2019-02-20 13:47:28 +0300

    added toast to feature2

--- app/src/main/java/com/example/user/epam2019/MainActivity.java
+++ app/src/main/java/com/example/user/epam2019/MainActivity.java
@@ -10,8 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(getApplicationContext(), "first feature1", Toast.LENGTH_LONG).show();
-        Toast.makeText(getApplicationContext(), "first feature1", Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "first feature2", Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "second feature2", Toast.LENGTH_LONG).show();
     }
 
 }
