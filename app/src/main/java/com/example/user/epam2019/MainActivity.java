266bed6da1855efb82812d316552069898e88ee5
AntonLiaskevich <antlon.lesk@gmail.com>
2019-02-22 13:50:25 +0300

    using stash in master branch

--- app/src/main/java/com/example/user/epam2019/MainActivity.java
+++ app/src/main/java/com/example/user/epam2019/MainActivity.java
@@ -10,8 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(), "first feature2", Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "second feature2", Toast.LENGTH_LONG).show();
         Toast.makeText(getApplicationContext(), "first feature1", Toast.LENGTH_LONG).show();
-        Toast.makeText(getApplicationContext(), "first feature1", Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "second feature1", Toast.LENGTH_LONG).show();
+
+        Toast.makeText(getApplicationContext(), "stash", Toast.LENGTH_LONG).show();
+
+
+
     }
 
 }
